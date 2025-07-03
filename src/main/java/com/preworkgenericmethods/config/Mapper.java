b3495609bd8bca994be155c6c8b33e251f9de273
sahla <sahlanerdem@gmail.com>
2025-07-03 22:26:44 +0300

    generic metodlarda mapleme sorunu fix

--- src/main/java/com/preworkgenericmethods/config/Mapper.java
+++ src/main/java/com/preworkgenericmethods/config/Mapper.java
@@ -1,6 +1,10 @@
 package com.preworkgenericmethods.config;
 
+import com.preworkgenericmethods.dto.StudentDTO;
+import com.preworkgenericmethods.models.Department;
+import com.preworkgenericmethods.models.Student;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
 import org.modelmapper.convention.MatchingStrategies;
 
 import java.util.Collection;
@@ -14,6 +18,7 @@ public class Mapper {
         modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
     }
+
     static {
         System.setProperty("org.modelmapper.internal.typetools.unsafe.disabled", "true");
         modelMapper = new ModelMapper();
@@ -22,25 +27,30 @@ public class Mapper {
                 .setFieldMatchingEnabled(true)
                 .setSkipNullEnabled(true)
                 .setAmbiguityIgnored(true);
+        modelMapper.createTypeMap(StudentDTO.class, Student.class)
+                .addMappings(mapper -> {
+                    mapper.skip(Student::setId);
+                    mapper.using(ctx -> {
+                        Long deptId = (Long) ctx.getSource();
+                        if (deptId == null) return null;
+                        Department dept = new Department();
+                        dept.setId(deptId);
+                        return dept;
+                    }).map(StudentDTO::getDepartmentId, Student::setDepartment);
+                });
+        modelMapper.createTypeMap(Student.class, StudentDTO.class)
+                .addMapping(src -> src.getDepartment().getId(), StudentDTO::setDepartmentId);
     }
-//    private static ModelMapper createModelMapper() {
-//        ModelMapper mapper = new ModelMapper();
-//        mapper.getConfiguration()
-//                .setMatchingStrategy(MatchingStrategies.STRICT)
-//                .setFieldMatchingEnabled(true)
-//                .setSkipNullEnabled(true)
-//                .setAmbiguityIgnored(true);
-//        return mapper;
-//    }
-    public static <D,T> D map(final T entity,Class<D> outClass) {
+
+    public static <D, T> D map(final T entity, Class<D> outClass) {
         return modelMapper.map(entity, outClass);
     }
 
-    public static <D,T> List<D> mapAll(final Collection<T> entityList, Class<D> outClass){
-        return entityList.stream().map(e -> map(e,outClass)).collect(Collectors.toList());
+    public static <D, T> List<D> mapAll(final Collection<T> entityList, Class<D> outClass) {
+        return entityList.stream().map(e -> map(e, outClass)).collect(Collectors.toList());
     }
 
-    public static <S,D> D map(final S source,D destination) {
+    public static <S, D> D map(final S source, D destination) {
         modelMapper.map(source, destination);
         return destination;
     }
